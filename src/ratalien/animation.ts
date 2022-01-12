7026cfac403a8d70d1cf6e044029ddbb828623fa
OValya <valya.eto.ya@gmail.com>
2022-01-12 22:52:59 +0300

    add explosion

---
+++ src/ratalien/animation.ts
@@ -0,0 +1,96 @@
+const options = [
+  {
+    name: 'explosion',
+    keyCode: 'boom',  //????
+    src: './ratalien/sprites/explosion.png',
+    width: 851,
+    height: 158,
+    numberOfFrames: 6,
+  },
+  /*{
+    name: 'left',
+    keyCode: 'KeyA',
+    src: './assets/worm-walks-left-100.png',
+    width: 1442,
+    height: 100,
+    numberOfFrames: 15,
+  },
+  {
+    name: 'jump',
+    keyCode: 'KeyW',
+    src: './assets/worm-jump-100.png',
+    width: 107,
+    height: 100,
+    numberOfFrames: 2,
+  },*/
+]
+
+class AnimationBoom {
+  spritesheet: HTMLImageElement;
+  width: number;
+  height: number;
+  numberOfFrames: number;
+  frameIndex: number;
+  isStarted: boolean;
+  scale: number;
+  constructor(imageURL, width, height, numberOfFrames, scale = 1) {
+    let spritesheet = new Image();
+    spritesheet.src = imageURL;
+    this.spritesheet = spritesheet;
+    this.width = width;
+    this.height = height;
+    this.numberOfFrames = numberOfFrames || 1;
+    this.frameIndex = 0;
+    this.isStarted = false;
+    this.scale = scale
+  }
+  start(/*keyCode*/) {
+    this.isStarted = true;
+    this.frameIndex = 0;
+    // if (keyCode) {
+    //   this.setOptions(keyCode);
+    // }
+  }
+
+  stop() {
+    this.isStarted = false;
+  }
+
+  update(deltaTime) {  
+    this.frameIndex += deltaTime * 3;  //?????
+    if (this.frameIndex >= this.numberOfFrames) {
+      this.frameIndex = 0;
+    }
+  }
+
+  drawFrame(context, frame, x, y) {  //frame - номер кадра
+    context.drawImage(this.spritesheet,
+                      frame * this.width / this.numberOfFrames,
+                      0,
+                      this.width / this.numberOfFrames,
+                      this.height,
+                      x - (this.width / this.numberOfFrames / this.scale) / 2,
+                      y - (this.height / this.scale) / 2,
+                      (this.width / this.numberOfFrames) / this.scale,
+                      this.height / this.scale)
+  }
+
+  drawCurrentFrame(context, x, y){
+    this.drawFrame(context, Math.trunc(this.frameIndex), x, y)
+  }
+
+  render(context, deltaTime, position){
+    if (this.isStarted) {
+      this.update(deltaTime);
+    }
+    this.drawCurrentFrame(context, position.x, position.y);
+  }
+
+  setOptions(keyCode) {
+    const currentOptions = options.filter(it => it.keyCode === keyCode)[0];
+    this.spritesheet.src = currentOptions.src;
+    this.width = currentOptions.width;
+    this.height = currentOptions.height;
+    this.numberOfFrames = currentOptions.numberOfFrames;
+  }
+} 
